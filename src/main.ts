43abaff9d8340dfc6a6a39fb90c5b9523b30b24e
hoangnv <hoangnv@dgroup.co>
2026-05-10 16:45:52 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = Number(process.env.PORT ?? 3000);
-  await app.listen(port);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
